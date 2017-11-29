a9ccbfeceff3fc5a3acfb6aa9792c797c9c1bc53
Chris <chris@wearephat.uk>
2017-11-29 12:25:57 +0000

    New Project

--- sketch.js
+++ sketch.js
@@ -1,38 +1,57 @@
-let tileSize = 50;
+//https://github.com/stavrosdidakis/DAT-GAD-405_2017
+//https://p5js.org
+
+//DAT405 / GAD405
+//08_Universe Interaction
+
+//Create a variable that will store the selection number
+//that defines the shape (via keyboard)
+let selection = 0;
+let canvas;
 
 function setup() {
-  var canvas = createCanvas(500, 500);
-  // Move the canvas so it's inside our <div id="sketch-holder">.
-  canvas.parent('sketch-holder');
+  canvas = createCanvas(1000, 500);
+  canvas.parent('myContainer');
+  strokeWeight(8);
+  noFill();
+  frameRate(10);
+  //noLoop();
 }
 
-function draw(){
-  background(0); //Set your background color to black (0)
+function draw() {
+  //background(50);
+  //Create 300 shapes using the for-loop
+  for (var i = 0; i < 300; i++) {
+    push(); //store the matrix before translate
+    //Random position between the size of the canvas
+    translate(random(-100, width+100), random(-100, height+100));
+    //Random rotation
+    rotate(random(2*PI));
+    //Random scale
+    scale(random(3));
 
-  //These 3 variables hold the mapped values from the mouse position (from coordinates to 16bit)
-  var mouse1 = map(mouseY, 0, height, 0, 255);
-  var mouse2 = map(mouseX, 0, width, 0, 255);
-  var mouse3 = map(mouseY, 0, width, 255, 0);
+    //Create two variables that store the mapped values for mouse position
+    let makeRed = map(mouseX, 0, width, 0, 255);
+    let makeBlue = map(mouseY, 0, height, 0, 255);
 
-  //Four different values are created. The alpha channel is controlled
-  //by the mouse variables we created before
-  var color1 = color(255, 37, 54, 255);
-  var color2 = color(36, 194, 203, mouse1);
-  var color3 = color(36, 194, 203, mouse2);
-  var color4 = color(239, 108, 46, mouse3);
+    //Mouse interaction defines the color of the strokes
+    stroke(makeRed, random(255), makeBlue);
+    //The fill color is black, but with a transparency variation
+    fill(0, random(255));
 
-  //The nested for-loop creates the grid, and according to the position of the mouse,
-  //the fill color of the shape will be different
-  for (let y = 0; y < height; y+=tileSize) {
-    for (let x = 0; x < width; x+=tileSize) {
-      //Create a conditional statement that it will check the coordinates of the mouse
-      //in relation to the position of the shapes on the grid, and according to this
-      //a color value will be selected
-      if(mouseX>x && mouseX<x+tileSize && mouseY>y && mouseY<y+tileSize) fill(color1);
-      else if (mouseX>x && mouseX<x+tileSize) fill(color2);
-      else if (mouseY>y && mouseY<y+tileSize) fill(color3);
-      else fill(color4);
-      rect(x, y, tileSize, tileSize);
-    }
+    //Selection of colors
+    //Based on the key pressed (defined outside the for-loop),
+    //the selection variable will either be 0, 1, or 2.
+    //This selected one of the colors defined below
+    if (selection===0) ellipse(0, 0, 40, 40);
+    if (selection===1) rect(0, 0, 40, 40);
+    if (selection===2) triangle(30, 75, 58, 20, 86, 75);
+
+    pop(); //reset the matrix to original state
   }
+
+  //According to the key pressed, change the number of the selection variable
+  if (key === 'a') selection = 0;
+  if (key === 's') selection = 1;
+  if (key === 'd') selection = 2;
 }

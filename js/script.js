9793b0abea1f1c22288c1eb75ea677942ff48e9e
Chris <chris@wearephat.uk>
2018-02-12 09:34:41 +0000

    insta

--- js/script.js
+++ js/script.js
@@ -5,35 +5,16 @@ var feed = new Instafeed({
   });
 feed.run();
 
-$(document).ready(function() {
+$(document).ready(function(){
+    $('.social-feed-container').socialfeed({
+        // INSTAGRAM
+        instagram:{
+            accounts: ['@teslamotors','#teslamotors'],  //Array: Specify a list of accounts from which to pull posts
+            limit: 2,                                    //Integer: max number of posts to load
+            access_token: '2242907265.5a4266b.999f81975caa49708e4de857382d0ce2' //String: Instagram access token
+        },
 
-    var updateFeed = function() {
-        $('.social-feed-container').socialfeed({
-            // Twitter
-            twitter: {
-                accounts: '@chrisOneOut',
-                limit: 2,
-                consumer_key: 'qzRXgkI7enflNJH1lWFvujT2P', // make sure to have your app read-only
-                consumer_secret: '8e7E7gHuTwyDHw9lGQFO73FcUwz9YozT37lEvZulMq8FXaPl8O', // make sure to have your app read-only
-            },
-            /* INSTAGRAM
-            instagram: {
-                accounts: '@chrisOneOut',
-                limit: 2,
-                access_token: '2242907265.5a4266b.999f81975caa49708e4de857382d0ce2'
-            }, */
-            // GENERAL SETTINGS
-            length: 200,
-            show_media: true,
-            // Moderation function - if returns false, template will have class hidden
-            moderation: function(content) {
-                return (content.text) ? content.text.indexOf('fuck') == -1 : true;
-            },
-            //update_period: 5000,
-            // When all the posts are collected and displayed - this function is evoked
-            callback: function() {
-                console.log('all posts are collected');
-            }
-        });
-    };
+        // GENERAL SETTINGS
+        length:400                                      //Integer: For posts with text longer than this length, show an ellipsis.
+    });
 });

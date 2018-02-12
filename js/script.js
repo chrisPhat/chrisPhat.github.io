52e64754ebce215bd02070ca251ae43a4d0cc315
Chris <chris@wearephat.uk>
2018-02-12 10:45:05 +0000

    example feeds upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
-$('.social-feed-container').socialfeed({
+var feed = new Instafeed({
+    get: 'user',
+    userId: '2242907265',
+    accessToken: '2242907265.1677ed0.a3c26012ce7a4a479d176c70e2a2c13e'
+  });
+feed.run();
 
-    // INSTAGRAM
-    instagram:{
-        accounts: ['@teslamotors','#teslamotors'],  //Array: Specify a list of accounts from which to pull posts
-        limit: 2,                                   //Integer: max number of posts to load
-        access_token: '2242907265.5a4266b.999f81975caa49708e4de857382d0ce2' //String: Instagram access token
-    },
-
-    // GENERAL SETTINGS
-    length:400,                                     //Integer: For posts with text longer than this length, show an ellipsis.
-    show_media:true,                                //Boolean: if false, doesn't display any post images
-    update_period: 5000,                            //Integer: Number of seconds before social-feed will attempt to load new posts.
-    template_html:                                  //String: HTML used for each post. This overrides the 'template' filename option
-    '<article class="twitter-post"> \
-    <h4>{{=it.author_name}}</h4><p>{{=it.text}}  \
-    <a href="{{=it.link}}" target="_blank">read more</a> \
-    </p> \
-    </article>',
-    callback: function() {                          //Function: This is a callback function which is evoked when all the posts are collected and displayed
-        console.log("All posts collected!");
-    }
+$(document).ready(function() {
+  $('#tweecool').tweecool({
+  //settings
+  username : 'chrisOneOut',
+  limit : 5
+  });
 });

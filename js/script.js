54327da0bb674f19de0d516a4236d5b2d5d113d3
Chris <chris@wearephat.uk>
2018-02-12 09:21:12 +0000

    Social Feed 3

--- js/script.js
+++ js/script.js
@@ -8,43 +8,21 @@ feed.run();
 $(document).ready(function() {
 
     var updateFeed = function() {
-        var initialQuery = $('#query').val();
-        initialQuery = initialQuery.replace(" ", "");
-        var queryTags = initialQuery.split(",");
         $('.social-feed-container').socialfeed({
-            // FACEBOOK
-            facebook: {
-                accounts: queryTags,
-                limit: 2,
-                access_token: '150849908413827|a20e87978f1ac491a0c4a721c961b68c'
-            },
-            // GOOGLEPLUS
-            google: {
-                accounts: queryTags,
-                limit: 2,
-                access_token: 'AIzaSyDAelFmJhg6BSUbSLe8UT7s-G53tL4_KRg'
-            },
-            // Twitter
+            /* Twitter
             twitter: {
                 accounts: '@chrisOneOut',
                 limit: 2,
                 consumer_key: 'qzRXgkI7enflNJH1lWFvujT2P', // make sure to have your app read-only
                 consumer_secret: '8e7E7gHuTwyDHw9lGQFO73FcUwz9YozT37lEvZulMq8FXaPl8O', // make sure to have your app read-only
-            },
-            // VKONTAKTE
-            vk: {
-                accounts: queryTags,
-                limit: 2,
-                source: 'all'
-            },
+            },*/
             // INSTAGRAM
             instagram: {
                 accounts: '@chrisOneOut',
                 limit: 2,
-                client_id: '88b4730e0e2c4b2f8a09a6184af2e218',
-                access_token: ''
+                client_id: '999f81975caa49708e4de857382d0ce2',
+                access_token: '2242907265.5a4266b.999f81975caa49708e4de857382d0ce2'
             },
-
             // GENERAL SETTINGS
             length: 200,
             show_media: true,

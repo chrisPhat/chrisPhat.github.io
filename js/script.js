2f4a0f49734f4c887744960f88ee0d890f1315b4
Chris <chris@wearephat.uk>
2018-02-12 09:31:11 +0000

    twitter

--- js/script.js
+++ js/script.js
@@ -9,19 +9,19 @@ $(document).ready(function() {
 
     var updateFeed = function() {
         $('.social-feed-container').socialfeed({
-            /* Twitter
+            // Twitter
             twitter: {
                 accounts: '@chrisOneOut',
                 limit: 2,
                 consumer_key: 'qzRXgkI7enflNJH1lWFvujT2P', // make sure to have your app read-only
                 consumer_secret: '8e7E7gHuTwyDHw9lGQFO73FcUwz9YozT37lEvZulMq8FXaPl8O', // make sure to have your app read-only
-            },*/
-            // INSTAGRAM
+            },
+            /* INSTAGRAM
             instagram: {
                 accounts: '@chrisOneOut',
                 limit: 2,
                 access_token: '2242907265.5a4266b.999f81975caa49708e4de857382d0ce2'
-            },
+            }, */
             // GENERAL SETTINGS
             length: 200,
             show_media: true,
